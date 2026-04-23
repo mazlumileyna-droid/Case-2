e67b185f25fe3a1014f5ee26c44e3e2468e8b561
SanielaDhimitri <saniela0709@icloud.com>
2026-04-23 19:44:03 +0200

    added new class

---
+++ Case 2/Models/User.cs
@@ -0,0 +1,23 @@
+﻿namespace Case_2.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+        public string password { get; set; }
+
+        public User(int userId, string name, string email, string password)
+        {
+            this.userId = userId;
+            this.name = name;
+            this.email = email;
+            this.password = password;
+        }
+        public User()
+        {
+        }
+    }
+}
